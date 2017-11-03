194d1b0134ae0a3b5221f0e9feca2d513668e1cc
mark-alfonso <markkram.alfonso@gmail.com>
2017-11-04 00:26:16 +0800

    template strings

--- src/main.js
+++ src/main.js
@@ -1,23 +1,9 @@
- /* Rest */
+let name = 'Goo';
 
-function sum(...numbers) { // will put all the params to an array
-	return numbers.reduce(function(prev, current) {
-		return prev + current;
-	});
-}
+let template = `
+	<div class="Alert">
+		<p>${name}</p>
+	</div>
+`;
 
-function sum(...numbers) {
-	return numbers.reduce(
-		(prev, current) => prev + current
-	);
-}
-//console.log(sum(1, 2, 3, 4, 5));
-
-/* Spread */
-function mult(x, y) {
-	return x * y;
-}
-
-let nums = [1, 2];
-
-console.log(mult(... nums));
+console.log(template);
---
+++ src/rest-spread.js
@@ -0,0 +1,23 @@
+ /* Rest */
+
+function sum(...numbers) { // will put all the params to an array
+	return numbers.reduce(function(prev, current) {
+		return prev + current;
+	});
+}
+
+function sum(...numbers) {
+	return numbers.reduce(
+		(prev, current) => prev + current
+	);
+}
+//console.log(sum(1, 2, 3, 4, 5));
+
+/* Spread */
+function mult(x, y) {
+	return x * y;
+}
+
+let nums = [1, 2];
+
+console.log(mult(...nums));
